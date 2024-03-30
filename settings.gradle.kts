53468aea7d5aa5c8c7d48858fa4282af22b7ee32
Miyako <weilanxiaogz@outlook.com>
2024-03-30 12:40:39 +0800

    添加阿里云镜像仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,33 @@
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+  repositories {
+    maven {
+      content {
+        url = uri("https://maven.aliyun.com/nexus/content/groups/public/")
+      }
     }
+    google {
+      content {
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("androidx.*")
+      }
+    }
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    maven {
+      content {
+        url = uri("https://maven.aliyun.com/nexus/content/groups/public/")
+      }
     }
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "EF-Scaffold"
 include(":app")
- 
